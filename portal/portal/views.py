3bfae27ffc4b4f8b4de7d6d0438bb5cbc31a209e
cs2be <nguyenthuan@baidu.com>
2017-09-14 17:00:14 -0700

    intermediate check in of static file handler

--- portal/portal/views.py
+++ portal/portal/views.py
@@ -1,10 +1,17 @@
+import os
+import posixpath
+
 from django.template.loader import get_template
 from django.template.loader import render_to_string
 from django.http import HttpResponse
 from django.http import JsonResponse
 from django.shortcuts import render
 from django.conf import settings
+from django.utils.six.moves.urllib.parse import unquote
+from django.contrib.staticfiles import finders
 
+from django.http import Http404
+from django.views import static
 
 def catch_all_handler(request, path=None):
     # Resolve path based on template.
@@ -17,5 +24,48 @@ def catch_all_handler(request, path=None):
         # Return the index of the app's documentation.
 
         # TODO: Test nested paths to make sure they pull correctly
-        static_content_template = get_template(path + '/index.html')
+        static_content_template = get_template(path)
         return render(request, '_base.html', {'static_content': static_content_template.render()})
+
+def blog_root(request):
+    print "BLOG ROOT"
+    path = settings.EXTERNAL_TEMPLATE_DIR + "/blog/index.html"
+    static_content_template = get_template(path)
+    static_content = static_content_template.render()
+    return render(request, '_base.html', {'static_content': static_content})
+
+def documentation_root(request, language):
+    print "DOCUMENTATION ROOT"
+    path = "%s/documentation/%s/html/index.html" % (settings.EXTERNAL_TEMPLATE_DIR, language)
+    static_content_template = get_template(path)
+    static_content = static_content_template.render()
+    return render(request, '_base.html', {'static_content': static_content})
+
+def static_file_handler(request, path, extension, insecure=False, **kwargs):
+    """
+    Serve static files below a given point in the directory structure or
+    from locations inferred from the staticfiles finders.
+    To use, put a URL pattern such as::
+        from django.contrib.staticfiles import views
+        url(r'^(?P<path>.*)$', views.serve)
+    in your URLconf.
+    It uses the django.views.static.serve() view to serve the found files.
+    """
+    append_path = ""
+
+    print "CSS HANDLER! %s, %s" % (path, extension)
+    if not settings.DEBUG and not insecure:
+        raise Http404
+
+    normalized_path = posixpath.normpath(unquote(path)).lstrip('/')
+    print "normalized_path ROOT: %s" %(normalized_path)
+
+    # absolute_path = finders.find(normalized_path)
+    absolute_path = settings.EXTERNAL_TEMPLATE_DIR + "/" + append_path + normalized_path + "." + extension
+    print "ABS ROOT: %s" %(absolute_path)
+    if not absolute_path:
+        if path.endswith('/') or path == '':
+            raise Http404("Directory indexes are not allowed here.")
+        raise Http404("'%s' could not be found" % path)
+    document_root, path = os.path.split(absolute_path)
+    return static.serve(request, path, document_root=document_root, **kwargs)
