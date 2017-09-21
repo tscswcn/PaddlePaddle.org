6d195f05598eddd3a737755523c5f144aae6839e
cs2be <nguyenthuan@baidu.com>
2017-09-21 16:29:13 -0700

    add tag during docker build

--- scripts/travis/build-docker.sh
+++ scripts/travis/build-docker.sh
@@ -3,5 +3,22 @@ set -e
 
 # Builds the docker image for PaddlePaddle.org
 
+if [ "$TRAVIS_BRANCH" == "master" ]
+then
+    # Production Deploy
+    export DOCKER_IMAGE_TAG="latest"
+elif [ "$TRAVIS_BRANCH" == ^release* ]
+then
+    # Staging Deploy
+    export DOCKER_IMAGE_TAG="staging"
+elif [ "$TRAVIS_BRANCH" == "develop" ]
+then
+    # Development Deploy
+    export DOCKER_IMAGE_TAG="develop"
+else
+# All other branches should be ignored
+    exit 0
+fi
+
 docker --version  # document the version travis is using
-docker build -t paddlepaddle.org .
+docker build -t paddlepaddle.org:"$DOCKER_IMAGE_TAG" .
