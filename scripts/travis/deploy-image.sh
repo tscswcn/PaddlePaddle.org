2bc5f404ddb89d9b6fff717fd110228c8942e4a9
Thuan Nguyen <cs2be@yahoo.com>
2017-09-25 14:10:35 -0700

    Update styles for navigation (#73) (#74)

--- scripts/travis/deploy-image.sh
+++ scripts/travis/deploy-image.sh
@@ -63,6 +63,7 @@ ssh -i ubuntu.pem ubuntu@$STAGE_DEPLOY_IP << EOF
   docker stop $DOCKER_CONTAINER_NAME
   docker rm $DOCKER_CONTAINER_NAME
   docker run --name=$DOCKER_CONTAINER_NAME -d -p $PORT:8000 -e ENV=$ENV -e SECRET_KEY=$SECRET_KEY -v /var/content:/var/content 330323714104.dkr.ecr.ap-southeast-1.amazonaws.com/paddlepaddle.org:$DOCKER_IMAGE_TAG
+  docker image prune
 EOF
 
 chmod 644 ubuntu.pem
