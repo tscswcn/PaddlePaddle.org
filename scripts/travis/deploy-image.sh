0b534d52980eeadeca2c01c509484957dcc141f0
Thuan Nguyen <cs2be@yahoo.com>
2017-09-25 17:28:28 -0700

    Release 0.2.4 (#79)
    * Update styles for navigation (#73)
    
    * remove header for book (#75)
    
    * Update top navigation link styles (#77)

--- scripts/travis/deploy-image.sh
+++ scripts/travis/deploy-image.sh
@@ -63,7 +63,7 @@ ssh -i ubuntu.pem ubuntu@$STAGE_DEPLOY_IP << EOF
   docker stop $DOCKER_CONTAINER_NAME
   docker rm $DOCKER_CONTAINER_NAME
   docker run --name=$DOCKER_CONTAINER_NAME -d -p $PORT:8000 -e ENV=$ENV -e SECRET_KEY=$SECRET_KEY -v /var/content:/var/content 330323714104.dkr.ecr.ap-southeast-1.amazonaws.com/paddlepaddle.org:$DOCKER_IMAGE_TAG
-  docker image prune
+  docker image prune -f
 EOF
 
 chmod 644 ubuntu.pem
