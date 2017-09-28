c72b56cd68e80e062e7acbd15928e0bbeee3f01c
Thuan Nguyen <cs2be@yahoo.com>
2017-09-27 19:05:26 -0700

    Restyle2 (#82)
    * add invert header/footer for blog, add active class to selected categories
    
    * initial update of home page

--- portal/portal/templatetags/app_tags.py
+++ portal/portal/templatetags/app_tags.py
@@ -77,14 +77,30 @@ TUTORIAL_NAV_DATA = {
 
 register = template.Library()
 
+
 @register.filter(name='links')
 def links(chapter):
     return map(lambda s: s['link'], chapter['sections'])
 
 
+@register.simple_tag(takes_context=True)
+def apply_class_if_template(context, template_file_name, class_name):
+    '''
+    Function that returns 'active' if the current base template matches the passed in template name, otherwise return
+    empty string.  This method is used to apply the "active" class to the root navigation links
+    :param context:
+    :param template_file_name:
+    :return:
+    '''
+    if context.template.name == template_file_name:
+        return class_name
+    else:
+        return ''
+
 @register.inclusion_tag('_content_links.html', takes_context=True)
 def content_links(context, book_id):
     #TODO[thuan]: Load content links for book with id
+
     return {
         'request': context.request,
         'side_nav_content': TUTORIAL_NAV_DATA
