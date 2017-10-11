fff166b902fb750a5e06320cb73696ef9f46f856
Wang,Jeff <wangjeff@baidu.com>
2017-10-10 18:25:33 -0700

    Update the script to use tmp directory Skip the content if the content repo doesn't support particular version Better error handling. Clean up the temp directory after use

--- portal/portal/management/commands/pull_docs.py
+++ portal/portal/management/commands/pull_docs.py
@@ -1,68 +1,94 @@
-from django.core.management import BaseCommand
-from git import Repo
 import os
 import shutil
+import tempfile
+
+from exceptions import OSError
+from django.core.management import BaseCommand
+from git import Repo
+from git.exc import GitCommandError
 
 
-#The class must be named Command, and subclass BaseCommand
+# The class must be named Command, and subclass BaseCommand
 class Command(BaseCommand):
     # Show this when the user types help
     help = "Usage: python manage.py pull_docs <version>"
 
     def add_arguments(self, parser):
         parser.add_argument('version', nargs='+')
-        parser.add_argument('dest_dict', nargs='+', )
+        parser.add_argument('dest_dir', nargs='+', )
 
     # A command must define handle()
     def handle(self, *args, **options):
         book_repo_path = 'git@github.com:PaddlePaddle/book.git'
         models_repo_path = 'git@github.com:PaddlePaddle/models.git'
 
-        temp_dict = 'temp'
-        book_dest = 'temp/book'
-        models_dest = 'temp/models'
+        temp_dir = '%s/tmp_repo' % tempfile.gettempdir()
+        book_dir = '%s/book' % temp_dir
+        models_dir = '%s/models' % temp_dir
 
-        # Clone Book repo
+        print 'Temp working dir: %s' % temp_dir
         try:
-            Repo.clone_from(book_repo_path, book_dest)
-        except:
-            print "Already cloned book repo"
+            # Clone Repos
+            print "Pulling repos"
+            Repo.clone_from(book_repo_path, book_dir)
+            Repo.clone_from(models_repo_path, models_dir)
 
-        # Clone Models repo
-        try:
-            Repo.clone_from(models_repo_path, models_dest)
-        except:
-            print "Already cloned models repo"
+            # Set dest_dict
+            dest_dir_list = options['dest_dir']
+            dest_dir = dest_dir_list[0] if dest_dir_list else None
 
-        # Set dest_dict
-        dest_dict_list = options['dest_dict']
-        dest_dict = dest_dict_list[0] if dest_dict_list else None
+            if not dest_dir:
+                raise "No dest_dir specified"
+            else:
+                print "Destination: %s" % dest_dir
 
-        if dest_dict == None:
-            raise
-        else:
-            print "Destination: %s" % dest_dict
+            book_repo = Repo(book_dir)
+            models_repo = Repo(models_dir)
 
-        book_repo = Repo(book_dest)
-        models_repo = Repo(models_dest)
+            if 'version' in options:
+                for version in options['version']:
+                    print 'Prepare version: %s' % version
+                    version_dir = '%s/%s' % (dest_dir, version)
 
-        if 'version' in options:
-            for version in options['version']:
-                print 'Prepare version: %s' % version
-                version_dict = '%s/%s' % (dest_dict, version)
+                    # Create the dest folder
+                    if not os.path.exists(version_dir):
+                        print "Create dir at: %s" % version_dir
+                        os.makedirs(version_dir)
 
-                # Clean the dest folder
-                if not os.path.exists(version_dict):
-                    print "Create dir at: %s" % version_dict
-                    os.makedirs(version_dict)
+                    print "Start copying version: %s into dest dir" % version
 
-                book_repo.git.checkout(version)
-                models_repo.git.checkout(version)
+                    try:
+                        book_repo.git.checkout(version)
+                        book_doc_dest = '%s/book' % version_dir
+                        print "Copy Book data to %s" % book_doc_dest
+                        shutil.copytree(book_dir, book_doc_dest, symlinks=True)
+                    except GitCommandError:
+                        print "Skip Book content. Unable to checkout %s branch on Book repo!" % version
+                    except OSError:
+                        print "Skip Book content. Content exists already"
+                    except Exception as e:
+                        print e
+                        print "Skip Book content. Unknown exception"
 
-                if os.path.exists(version_dict):
-                    print "Remove all files at: %s" % version_dict
-                    shutil.rmtree(version_dict)
+                    try:
+                        models_repo.git.checkout(version)
+                        models_doc_dest = '%s/models' % version_dir
+                        print "Copy Models data to %s" % models_doc_dest
+                        shutil.copytree(models_dir, models_doc_dest, symlinks=True)
+                    except GitCommandError:
+                        print "Skip Models content. Unable to checkout %s branch on Models repo!" % version
+                    except OSError:
+                        print "Skip Models content. Content exists already"
+                    except Exception as e:
+                        print e
+                        print "Skip Models content. Unknown exception"
 
-                print "Start copying"
-                shutil.copytree(temp_dict, version_dict, symlinks=True)
-
+        except Exception as e:
+            print e.__class__
+            print e
+        finally:
+            # Clean up the temp folder
+            if os.path.exists(temp_dir):
+                print 'Remove temp content dir'
+                shutil.rmtree(temp_dir)
+            print "BYE"
