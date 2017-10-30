6560467bb976a07982fc3867a07196f6e9abe1ab
daming-lu <daminglu@yahoo.com>
2017-10-30 15:08:15 -0700

    debug blog travis

--- scripts/deploy/generate_blog_docs.sh
+++ scripts/deploy/generate_blog_docs.sh
@@ -7,6 +7,10 @@ DESTINATION_DIR=$2
 echo "Generating blog documentation at $DOCS_LOCATION to $DESTINATION_DIR"
 cd "$DOCS_LOCATION"
 
+which bundle
+echo "listing from within PPO"
+gem list
+
 bundle exec jekyll build
 
 cp -r ./_site/ $DESTINATION_DIR
