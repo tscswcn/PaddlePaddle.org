9e7423f0fe8402f348f8ebe34ac8a456c2214449
daming-lu <daminglu@yahoo.com>
2017-11-10 14:15:50 -0800

    test cron job

---
+++ scripts/travis/upload_workspace_to_s3.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+set -e
+
+# deploy to remote server
+openssl aes-256-cbc -d -a -in $TRAVIS_BUILD_DIR/scripts/travis/ubuntu.pem.enc -out ubuntu.pem -k $DEC_PASSWD
+
+eval "$(ssh-agent -s)"
+chmod 400 ubuntu.pem
+
+ssh-add ubuntu.pem
+
+ssh -i ubuntu.pem ubuntu@$STAGE_DEPLOY_IP << EOF
+  set -e
+
+  cd /var/content
+  tar -czvf workspace.tar.gz .ppo_workspace/
+  aws s3 cp workspace.tar.gz s3://paddlepaddle.org/workspace.tar.gz --grants read=uri=http://acs.amazonaws.com/groups/global/AllUsers
+
+EOF
+
+chmod 644 ubuntu.pem
+rm ubuntu.pem
