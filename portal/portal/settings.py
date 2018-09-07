8b658661797ce476df65e1ff22d30d5bae5f11e7
Jeff Wang <wangjeff@baidu.com>
2018-09-07 07:47:30 -0700

    Include the new 0.15.0 version (#644)

--- portal/portal/settings.py
+++ portal/portal/settings.py
@@ -109,10 +109,10 @@ VERSIONS = [
     { 'name': '0.12.0', 'api': 'v2' },
     { 'name': '0.13.0', 'api': 'Fluid' },
     { 'name': '0.14.0', 'api': 'Fluid' },
+    { 'name': '0.15.0', 'api': 'Fluid' },
 ]
 
 if DEBUG:
-    VERSIONS.append({ 'name': '0.15.0', 'api': 'Fluid' })
     VERSIONS.append({ 'name': 'develop_doc', 'api': 'Fluid' })
     VERSIONS.append({ 'name': 'develop', 'api': 'Fluid' })
 
